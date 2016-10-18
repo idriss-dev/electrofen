4c66cffd0492f29ea32013d6687ffd181af63344
donkakashi <idriss0said0alaoui@gmail.com>
2016-10-18 13:57:14 +0100

    updated the game to include the little girl

--- core/src/com/mygdx/game/ElectroFunCop22.java
+++ core/src/com/mygdx/game/ElectroFunCop22.java
@@ -10,7 +10,9 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
@@ -18,104 +20,230 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 
 public class ElectroFunCop22 extends ApplicationAdapter {
-   private Texture dropImage;
-   private Texture bucketImage;
-   private SpriteBatch batch;
-   private OrthographicCamera camera;
-   private Rectangle bucket;
-   private Array<Rectangle> raindrops;
-   private Array<Rectangle> buckets;
-   private long lastDropTime;
+	private static final int        FRAME_GIRL_COLS = 4;         // #1
+    private static final int        FRAME_GIRL_ROWS = 4;         // #2
+    
+    private static final int        HEIGHT = 600;         
+    private static final int        WIDTH = 600;
+    
+    private static final int        TILE_UNIT = 25;
+    
+    Animation                       girlRightAnimation;  
+    Animation                       girlLeftAnimation; 
+    Animation                       girlTopAnimation; 
+    Animation                       girlBottomAnimation; 
+    
+    Texture                         girlSheet;              // #4
+    Texture                         grassImage; 
+    Texture                         moulinImage;              // #4
+    Texture                         houseImage;
+    Texture                         waterImage;
+    Texture                         treeImage;
+    
+    TextureRegion[]                 girlRightFrames;   
+    TextureRegion[]                 girlLeftFrames;
+    TextureRegion[]                 girlTopFrames;
+    TextureRegion[]                 girlBottomFrames;
+    
+    SpriteBatch                     spriteBatch;            
+    TextureRegion                   currentGirlFrame;       
+    
+    Array<Rectangle> grassList;
+    Array<Rectangle> waterList;
+    Array<Rectangle> treeList;
+    
+    Rectangle moulin;
+    Rectangle house;
+    
+    float girlSpeed = 100.0f; // 100 pixels per second.
+    float girlX = HEIGHT / 2;
+    float girlY = WIDTH / 2;
 
    @Override
    public void create() {
-      // load the images for the droplet and the bucket, 64x64 pixels each
-      dropImage = new Texture(Gdx.files.internal("droplet.png"));
-      bucketImage = new Texture(Gdx.files.internal("bucket.png"));
-
-      // create the camera and the SpriteBatch
-      camera = new OrthographicCamera();
-      camera.setToOrtho(false, 800, 480);
-      batch = new SpriteBatch();
-      // create the raindrops array and spawn the first raindrop
-      raindrops = new Array<Rectangle>();
-      buckets = new Array<Rectangle>();
+	   girlSheet = new Texture(Gdx.files.internal("girl.png")); // #9
+	   grassImage = new Texture(Gdx.files.internal("grass.jpg"));
+	   moulinImage = new Texture(Gdx.files.internal("moulin.png"));
+	   houseImage = new Texture(Gdx.files.internal("house.png"));
+	   waterImage = new Texture(Gdx.files.internal("water.png"));
+	   treeImage = new Texture(Gdx.files.internal("tree.png"));
+	   
+       TextureRegion[][] tmp = TextureRegion.split(girlSheet, girlSheet.getWidth()/FRAME_GIRL_COLS, girlSheet.getHeight()/FRAME_GIRL_ROWS);              // #10
+       
+       girlRightFrames = new TextureRegion[FRAME_GIRL_ROWS];
+       girlLeftFrames = new TextureRegion[FRAME_GIRL_ROWS];
+       girlTopFrames = new TextureRegion[FRAME_GIRL_ROWS];
+       girlBottomFrames = new TextureRegion[FRAME_GIRL_ROWS];
+       
+       int index = 0;
+       for (int j = 0; j < FRAME_GIRL_COLS; j++) {
+    	   girlBottomFrames[index++] = tmp[0][j];
+       }
+       index = 0;
+       for (int j = 0; j < FRAME_GIRL_COLS; j++) {
+    	   girlLeftFrames[index++] = tmp[1][j];
+       }
+       index = 0;
+       for (int j = 0; j < FRAME_GIRL_COLS; j++) {
+    	   girlRightFrames[index++] = tmp[2][j];
+       }
+       index = 0;
+       for (int j = 0; j < FRAME_GIRL_COLS; j++) {
+    	   girlTopFrames[index++] = tmp[3][j];
+       }
+       
+       girlRightAnimation = new Animation(0.1f, girlRightFrames);      
+       girlLeftAnimation = new Animation(0.1f, girlLeftFrames);
+       girlBottomAnimation = new Animation(0.1f, girlBottomFrames);
+       girlTopAnimation = new Animation(0.1f, girlTopFrames);
+       
+       grassList = new Array<Rectangle>();
+       waterList = new Array<Rectangle>();
+       treeList = new Array<Rectangle>();
+       
+       renderGround();
+       renderHouse();
+       renderMoulin();
+       renderWater();
+          
+       spriteBatch = new SpriteBatch();    
    }
 
-   private void spawnRaindrop() {
-      Rectangle raindrop = new Rectangle();
-      raindrop.x = MathUtils.random(0, 800-64);
-      raindrop.y = MathUtils.random(0, 800-64);
-      raindrop.width = 64;
-      raindrop.height = 64;
-      raindrops.add(raindrop);
+   private void renderRandomTree() {
+      Rectangle tree = new Rectangle();
+      tree.x = MathUtils.random(0, WIDTH-64);
+      tree.y = MathUtils.random(0, HEIGHT-64);
+      tree.width = 64;
+      tree.height = 64;
+      treeList.add(tree);
    }
    private void spawnBucket() {
-      Rectangle bucket = new Rectangle();
-      bucket.x = MathUtils.random(0, 800-64);
-      bucket.y = MathUtils.random(0, 800-64);
-      bucket.width = 64;
-      bucket.height = 64;
-      buckets.add(bucket);
+//      Rectangle bucket = new Rectangle();
+//      bucket.x = MathUtils.random(0, 800-64);
+//      bucket.y = MathUtils.random(0, 800-64);
+//      bucket.width = 64;
+//      bucket.height = 64;
+//      buckets.add(bucket);
    }
+   
+   private void renderGround() {
+	   for(int i = 0; i < HEIGHT; i += TILE_UNIT){
+		   for(int j = 0; j < WIDTH; j += TILE_UNIT){
+			  Rectangle grass = new Rectangle();
+		      grass.x = i;
+		      grass.y = j;
+		      grass.width = TILE_UNIT;
+		      grass.height = TILE_UNIT;
+		      grassList.add(grass);
+		   }
+	   }
+      
+   }
+   
+   private void renderWater() {
+	   int startIdx = WIDTH/2;
+	   int endIdx = startIdx + 20;
+	   Rectangle water;
+	   
+	   for(int i = 0; i < HEIGHT; i += TILE_UNIT){
+		   water = new Rectangle();
+		   water.x = startIdx;
+	       water.y = i;
+	       water.width = TILE_UNIT;
+	       water.height = TILE_UNIT;
+	       waterList.add(water);
+	       
+	       water = new Rectangle();
+		   water.x = startIdx + 25;
+	       water.y = i;
+	       water.width = TILE_UNIT;
+	       water.height = TILE_UNIT;
+	       waterList.add(water);
+	   }
+   }
+   
+   private void renderMoulin() {
+	  moulin = new Rectangle();
+	  moulin.x = 400;
+	  moulin.y = 400;
+   }
+   
+   private void renderHouse() {
+	  house = new Rectangle();
+	  house.x = 50;
+	  house.y = 300;
+   }
+   
 
    @Override
    public void render() {
-      // clear the screen with a dark blue color. The
-      // arguments to glClearColor are the red, green
-      // blue and alpha component in the range [0,1]
-      // of the color to be used to clear the screen.
-      Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-      Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-      // tell the camera to update its matrices.
-      camera.update();
-
-      // tell the SpriteBatch to render in the
-      // coordinate system specified by the camera.
-      batch.setProjectionMatrix(camera.combined);
-
-      // begin a new batch and draw the bucket and
-      // all drops
-      batch.begin();
-      for(Rectangle bucket: buckets) {
-         batch.draw(bucketImage, bucket.x, bucket.y);
-      }
-      for(Rectangle raindrop: raindrops) {
-          batch.draw(dropImage, raindrop.x, raindrop.y);
-      }
-      batch.end();
-
       if(Gdx.input.isKeyPressed(Keys.A)){
-    	  spawnBucket();
+    	  renderRandomTree();
       }
-      if(Gdx.input.isKeyPressed(Keys.B)){
-    	  spawnRaindrop();
-      }
+//      if(Gdx.input.isKeyPressed(Keys.B)){
+//    	  spawnRaindrop();
+//      }
       if(Gdx.input.isKeyPressed(Keys.C)){
-    	  	Iterator<Rectangle> iter = buckets.iterator();
+    	  	Iterator<Rectangle> iter = treeList.iterator();
 	        while(iter.hasNext()) {
 	           iter.next();
 	           iter.remove();
 	           break;
 	        }
       }
-      if(Gdx.input.isKeyPressed(Keys.D)){
-    	  Iterator<Rectangle> iter = raindrops.iterator();
-	        while(iter.hasNext()) {
-	           iter.next();
-	           iter.remove();
-	           break;
-	        }
-      }
+//      if(Gdx.input.isKeyPressed(Keys.D)){
+//    	  Iterator<Rectangle> iter = raindrops.iterator();
+//	        while(iter.hasNext()) {
+//	           iter.next();
+//	           iter.remove();
+//	           break; 
+//	        }
+//      }
+	   
+	   Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);                        
+	   girlSpeed += Gdx.graphics.getDeltaTime();           
+       
+	   currentGirlFrame = girlBottomAnimation.getKeyFrame(girlSpeed, true);
+       
+       if(Gdx.input.isKeyPressed(Keys.DPAD_LEFT)){
+    	   girlX -= Gdx.graphics.getDeltaTime() * girlSpeed;
+    	   currentGirlFrame = girlLeftAnimation.getKeyFrame(girlSpeed, true);
+       }
+	   if(Gdx.input.isKeyPressed(Keys.DPAD_RIGHT)){
+		   girlX += Gdx.graphics.getDeltaTime() * girlSpeed;
+		   currentGirlFrame = girlRightAnimation.getKeyFrame(girlSpeed, true);
+	   }
+	   if(Gdx.input.isKeyPressed(Keys.DPAD_UP)){
+		   girlY += Gdx.graphics.getDeltaTime() * girlSpeed;
+		   currentGirlFrame = girlTopAnimation.getKeyFrame(girlSpeed, true);
+	   }
+	   if(Gdx.input.isKeyPressed(Keys.DPAD_DOWN)){
+		   girlY -= Gdx.graphics.getDeltaTime() * girlSpeed;
+		   currentGirlFrame = girlBottomAnimation.getKeyFrame(girlSpeed, true);
+	   }
+	   
+       spriteBatch.begin();
+       for(Rectangle grassItem: grassList) {
+    	   spriteBatch.draw(grassImage, grassItem.x, grassItem.y);
+	   }
+       for(Rectangle waterItem: waterList) {
+    	   spriteBatch.draw(waterImage, waterItem.x, waterItem.y);
+	   }
+       
+       spriteBatch.draw(currentGirlFrame, girlX, girlY);
+       
+       for(Rectangle treeItem: treeList) {
+    	   spriteBatch.draw(treeImage, treeItem.x, treeItem.y);
+        }
+       
+       spriteBatch.draw(moulinImage, moulin.x, moulin.y);
+       spriteBatch.draw(houseImage, house.x, house.y);
+       spriteBatch.end();
 
    }
 
    @Override
    public void dispose() {
-      // dispose of all the native resources
-      dropImage.dispose();
-      bucketImage.dispose();
-      batch.dispose();
+      
    }
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Electrofun - cop22";
-		config.width = 800;
-	    config.height = 800;
+		config.width = 600;
+	    config.height = 600;
 		
 		new LwjglApplication(new ElectroFunCop22(), config);
 	}
